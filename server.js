4ddb9d73e4850a2076f41c46d38c3d13b1643e4f
Polagani97 <priyankapolagani12@gmail.com>
2022-03-13 01:33:57 +1100

    server.js  updated

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+console.time("web server started in ");
+
 var express = require('express')
 var app = express()
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
- res.send('hello world')
+
+    var msg = 'Page has been requested'
+    logActivities(['Server Activity : ']+[msg]);
+
+    res.send('hello world')
+
+    msg = 'Response set to hello world'
+    logActivities(['Server Activity : ']+[msg]);
+    
 })
+
 // list to a particular port
 app.listen(3000)
+
+// add the function that logs activities
+var logActivities = function () {
+    return console.log.apply(
+        console,
+        [new Date().toTimeString().slice(0,8)].concat(
+            Array.prototype.slice.call(arguments)
+        )
+    );
+};
+
+logActivities(['Server Activity']+[' :  Web server started']);
+
+console.timeEnd("web server started in ");
